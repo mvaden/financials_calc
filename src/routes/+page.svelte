03c79c2a14b0b6bcbf6910ee207ba38d1ed15c2e
mvaden <mvaden08@gmail.com>
2022-12-31 02:11:51 -0500

    feat: add base logic for new form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,33 @@
+<script>
+// @ts-nocheck
+
+   let principal = '';
+   let interestRate = '';
+   let years = '';
+
+   function submitForm() {
+      console.log(compoundedInterest(principal, interestRate / 100, years));
+   }
+
+   function compoundedInterest(principal, rate, year) {
+      const balanceHistory = [];
+
+      for (let i = 1; i <= year; i++) {
+         const interest = principal * rate;
+         principal += interest;
+         balanceHistory.push({
+            year: i,
+            balance: principal.toFixed(2),
+            interest: interest.toFixed(2),
+            newPrincipal: (principal - interest).toFixed(2)
+         });
+      }
+
+      return balanceHistory;
+   }
+
+</script>
+
 <h1>Welcome to Investment Calculator</h1>
 <p>Below is a calculator for estimating interest compounded investments over time. Simply enter in an amount, followed by the term (years) and the interest rate for that term.</p>
 <div>
